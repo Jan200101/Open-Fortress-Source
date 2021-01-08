6801f4e6c27df00e57903b1df15ad9ade2580571
Mangmasta <mangmasta@gmail.com>
2021-01-07 20:33:38 -0800

    Fixed chainsaw attack starting over halfway through charging.

--- src/game/shared/of/of_weapon_chainsaw.cpp
+++ src/game/shared/of/of_weapon_chainsaw.cpp
@@ -1,4 +1,4 @@
-//====== Copyright © 1996-2005, Valve Corporation, All rights reserved. =======
+//====== Copyright ï¿½ 1996-2005, Valve Corporation, All rights reserved. =======
 //
 // Purpose: 
 //
@@ -243,8 +243,8 @@ void CTFChainsaw::ItemPostFrame()
 	if ( !pOwner )
 		return;
 	
-	if ( m_iWeaponState > CS_IDLE && m_flNextPrimaryAttack < gpGlobals->curtime && !( pOwner->m_nButtons & IN_ATTACK ) )
-	{
+	if ( m_iWeaponState > CS_IDLE && m_flNextPrimaryAttack < gpGlobals->curtime && !( pOwner->m_nButtons & IN_ATTACK ) && !pOwner->m_Shared.InCond(TF_COND_SHIELD_CHARGE))
+	{	//if we are not idling, next primary attack is not up, player not pressing button, and not charging
 		SendWeaponAnim( ACT_MP_ATTACK_STAND_POSTFIRE );
 
 		// Set the appropriate firing state.
@@ -259,7 +259,6 @@ void CTFChainsaw::ItemPostFrame()
 	if (pOwner->m_Shared.InCond(TF_COND_SHIELD_CHARGE))
 	{
 		//HACK HACK HACK
-		pOwner->m_nButtons = IN_ATTACK;
 		PrimaryAttack();
 	}
 	BaseClass::ItemPostFrame();
